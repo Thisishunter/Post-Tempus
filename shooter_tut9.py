518c37cc6a42986d13c86edb6255fccdf6fc7776
Thisishunter <thisishunterhensley@gmail.com>
2024-06-21 19:08:07 -0700

    Update shooter_tut9.py

--- shooter_tut9.py
+++ shooter_tut9.py
@@ -10,7 +10,7 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Shooter')
+pygame.display.set_caption('Post Tempus')
 
 #set framerate
 clock = pygame.time.Clock()
